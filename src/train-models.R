fbce26aba98aa565c0d060209b0923c1c9b18aa4
Mark Timms <mtimms@protonmail.com>
2016-11-13 17:49:29 -0500

    Removed isolation forests and fit random forests

--- src/train-models.R
+++ src/train-models.R
@@ -1,29 +1,40 @@
-library(IsolationForest)
+# Program: train-models.R
+# Purpose: Fit a random forest model to each botnet dataset
+
+# Load packages -----------------------------------------------------------
+library(doMC)
 library(tidyverse)
+library(caret)
 
-set.seed(6246)
+# Register parallel backend -----------------------------------------------
+registerDoMC(5)
 
-fit_iso_forest <- function(data) {
-  
-  model <- data %>% 
-    select(-label) %>% 
-    IsolationTrees(ntree = 100, rowSamp = TRUE, nRowSamp = 256)
-  
-  scores <- data %>% 
-    select(-label) %>%
-    AnomalyScore(model)
-  
-  data %>% 
-    mutate(classification = ifelse(scores$outF <= 0.5, "normal", "botnet"))
-  
-}
-
+# Read data files ---------------------------------------------------------
 capture_files <- list.files("data", "\\.rds$", full.names = TRUE)
 
 capture_data <- capture_files %>% 
   map(read_rds) %>% 
-  set_names(gsub("^data/(.+)\\.rds$", "\\1", capture_files)) %>% 
-  parallel::mclapply(fit_iso_forest, mc.cores = 5)
+  set_names(gsub("^data/(.+)\\.rds$", "\\1", capture_files))
 
-capture_data %>% 
-  map(~ table(.x[["label"]], .x[["classification"]]))
+# Partition data into training/test sets ----------------------------------
+set.seed(6246)
+
+labeled_data <- map(capture_data, ~ filter(.x, label != "background"))
+
+partitions <- labeled_data %>% 
+  map(~ createDataPartition(.x[["label"]], p = 0.75, list = FALSE))
+
+train_data <- map2(labeled_data, partitions, ~ .x[.y, ])
+test_data  <- map2(labeled_data, partitions, ~ .x[-.y, ])
+
+# Fit models --------------------------------------------------------------
+ctrl <- trainControl("repeatedcv", 10, 10)
+
+models <- train_data %>% 
+  map(~ train(label ~ ., data = .x, trControl = ctrl, method = "rf"))
+
+# Evaluate model fits -----------------------------------------------------
+preds <- map2(models, test_data, predict)
+confusion_matrices <- map2(preds, test_data, ~ confusionMatrix(.x, .y[["label"]]))
+
+
