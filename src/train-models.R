a4976ba3f23b6a1db376ce4de07de7aa03cdf011
Mark Timms <mtimms@protonmail.com>
2016-11-15 22:17:20 -0500

    Fixed Isolation Forest Evaluation

--- src/train-models.R
+++ src/train-models.R
@@ -8,7 +8,7 @@ library(tidyverse)
 library(caret)
 
 # Register parallel backend -----------------------------------------------
-registerDoMC(2)
+registerDoMC(5)
 
 # Read data files ---------------------------------------------------------
 capture_files <- list.files("data", "\\.rds$", full.names = TRUE)
@@ -34,15 +34,18 @@ ctrl <- trainControl("repeatedcv", 10, 10)
 rf_models <- train_data %>% 
   map(~ train(label ~ ., data = .x, trControl = ctrl, method = "rf"))
 
-iso_models <- train_data %>% 
+if_models <- train_data %>% 
   map(~ select(.x, -label)) %>% 
-  map(IsolationTrees, ntree = 100, rowSamp = TRUE, nRowSamp = 256)
+  map(IsolationTrees, ntree = 1000, rowSamp = TRUE, nRowSamp = 256)
 
 # Evaluate model fits -----------------------------------------------------
 rf_pred <- map2(rf_models, test_data, predict)
-iso_pred <- map2(test_data, iso_models, AnomalyScore) %>% 
+if_pred <- map(test_data, ~ select(.x, -label)) %>% 
+  map2(if_models, AnomalyScore) %>% 
   map(~ ifelse(.x$outF >= 0.5, 1, 0))
 
 eval_rf <- map2(rf_pred, test_data, ~ confusionMatrix(.x, .y[["label"]]))
 eval_if <- map(test_data, "label") %>% 
-  map2(iso_pred, table)
+  map2(if_pred, table)
+
+
