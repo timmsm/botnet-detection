84720ae6fa9d1f5f85afc5fb55f3d5adf91a776c
Mark Timms <mtimms@protonmail.com>
2016-11-15 23:38:27 -0500

    Added k-means clustering

--- src/train-models.R
+++ src/train-models.R
@@ -2,6 +2,7 @@
 # Purpose: Fit a random forest model to each botnet dataset
 
 # Load packages -----------------------------------------------------------
+library(ClusterR)
 library(doMC)
 library(IsolationForest)
 library(tidyverse)
@@ -38,12 +39,32 @@ if_models <- capture_data %>%
   map(~ select(.x, -label)) %>% 
   map(IsolationTrees, ntree = 100, rowSamp = TRUE, nRowSamp = 256)
 
+km_models <- test_data %>% 
+  map(~ select(.x, -label) %>% as.matrix() %>% center_scale()) %>% 
+  map(KMeans_rcpp, clusters = 2, threads = 5)
+  
 # Evaluate model fits -----------------------------------------------------
 rf_pred <- map2(rf_models, test_data, predict)
 if_pred <- map(capture_data, ~ select(.x, -label)) %>% 
   map2(if_models, AnomalyScore) %>% 
   map(~ ifelse(.x$outF >= 0.5, 1, 0))
 
+km_pred <- test_data %>% 
+  map(~ select(.x, -label) %>% as.matrix() %>% center_scale()) %>%
+  map2(km_models, ~ predict_KMeans(.x, .y[["centroids"]]))
+
 eval_rf <- map2(rf_pred, test_data, ~ confusionMatrix(.x, .y[["label"]]))
 eval_if <- map(capture_data, "label") %>% 
   map2(if_pred, table)
+
+eval_km <- map(test_data, "label") %>% 
+  map2(km_pred, table)
+
+# Save models and evaluations ---------------------------------------------
+walk2(rf_models, names(rf_models), ~ write_rds(.x, sprintf("data/models/rf_%s.rds", .y)))
+walk2(if_models, names(if_models), ~ write_rds(.x, sprintf("data/models/if_%s.rds", .y)))
+walk2(km_models, names(km_models), ~ write_rds(.x, sprintf("data/models/km_%s.rds", .y)))
+
+walk2(eval_rf, names(eval_rf), ~ write_rds(.x, sprintf("data/evals/rf_%s.rds", .y)))
+walk2(eval_if, names(eval_if), ~ write_rds(.x, sprintf("data/evals/if_%s.rds", .y)))
+walk2(eval_km, names(eval_km), ~ write_rds(.x, sprintf("data/evals/km_%s.rds", .y)))
